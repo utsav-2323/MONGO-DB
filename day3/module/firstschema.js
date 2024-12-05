18707480f4676a9ac47ee9d0479fe931c6ae651e
utsav patel <utsacbhalodiya2323@gmail.com>
2024-12-05 14:49:30 +0530

    card-2

---
+++ day3/module/firstschema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    authername: { 
+        type: String, 
+        required: true 
+    },
+    price: { 
+        type: String, 
+        required: true 
+    },
+    publishdata: { 
+        type: String, 
+        required: true 
+    },
+});
+
+const admin = mongoose.model("Student",schema);
+module.exports = admin;
